286fafe3d8da3f5b49d19ac50b0ecc6513a5a6b4
Luis Garcia <luisgg@gmail.com>
2011-08-07 21:02:00 +0000

    WiP

---
+++ transmute/install/usr/share/aurex/base-files/etc/profile.sh
@@ -0,0 +1,11 @@
+# system-wide .profile file for AureX scripts
+
+for d in /usr/share/aurex/profile.d /etc/aurex/profile.d ; do
+	if [ -d "$d" ]; then
+		for f in /etc/profile.d/*.sh; do
+			[ ! -r "$f" ] || . $f
+		done
+		unset f
+	fi
+done
+unset d
---
+++ transmute/install/usr/share/aurex/profile.d/aurex-default-vars.sh
@@ -0,0 +1,5 @@
+AUREX_RELEASE="10.04"
+UBUNTU_CODENAME="lucid"
+AUREX_CODENAME="lucid"
+UBUNTU_DEFMIRROR="http://archive.ubuntu.com/ubuntu/"
+AUREX_DEFMIRROR="file:///srv/aurex/"
