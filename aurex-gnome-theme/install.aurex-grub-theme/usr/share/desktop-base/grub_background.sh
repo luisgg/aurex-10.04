f34754fcde3ff3d98dd4f6f7888d0849da01d77f
Luis Garcia <luisgg@gmail.com>
2011-08-21 22:41:41 +0000

    Added grub image

--- aurex-gnome-theme/install.aurex-grub-theme/usr/share/desktop-base/grub_background.sh
+++ aurex-gnome-theme/install.aurex-grub-theme/usr/share/desktop-base/grub_background.sh
@@ -1,3 +1,3 @@
-WALLPAPER="/usr/share/images/aurex/aurex-10.04-grub.png"
+WALLPAPER="/usr/share/pixmaps/aurex/aurex-10.04-grub.png"
 COLOR_NORMAL="white/black"
-COLOR_HIGHLIGHT="magenta/black"
+COLOR_HIGHLIGHT="black/yellow"
